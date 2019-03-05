22ec49ee08205f690433646905caa1351197f950
dana aljohar <crystalapple@Crystals-MacBook-Pro.local>
2019-03-05 18:44:01 +0300

    homework week 6

--- exercises/stop-light/index.js
+++ exercises/stop-light/index.js
@@ -1,23 +1,51 @@
 function turnOffLights() {
     // change background color of stop light to red
+    let stopLight = $("#stopLight");
+    stopLight.css("background","#111");
 }
-// turn on stop function 
+// turn on stop function
 function turnOnStopLight() {
+  turnOffLights();
+  let stopLight = $("#stopLight");
+  stopLight.css("background","red");
+  let slowLight = $("#slowLight");
+  slowLight.css("background","#111");
+  let goLight = $("#goLight");
+  goLight.css("background","#111");
     // change background color of stop light to red
 }
 
 // turn on slow function
-function turnOnSlowLight() { 
+function turnOnSlowLight() {
 // change background color of slow light to yellow
+turnOffLights();
+  let slowLight = $("#slowLight");
+  slowLight.css("background","yellow");
+  let stopLight = $("#stopLight");
+  stopLight.css("background","#111");
+  let goLight = $("#goLight");
+  goLight.css("background","#111");
 }
 
 // turn on go function
 function turnOnGoLight() {
     // change background color of go light to green
+    turnOffLights();
+    let goLight = $("#goLight");
+    goLight.css("background","green");
+    let stopLight = $("#stopLight");
+    stopLight.css("background","#111");
+    let slowLight = $("#slowLight");
+    slowLight.css("background","#111");
 }
 
 // set stop light click event
-
+$("#stopButton").on("click",turnOnStopLight);
 // set slow light click event
-
+$("#slowButton").on("click",turnOnSlowLight);
 // set go light click event
+$("#goButton").on("click",turnOnGoLight);
+
+turnOnStopLight();
+turnOnSlowLight();
+turnOnGoLight();
--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,22 @@
 // click event for gray button
-
+$("#grayButton").on("click",switchGray);
 // click event for white button
+$("#whiteButton").on("click",switchWhite);
+
 
 function switchGray() {
+let body =$("body");
+body.css({"background":"gray",
+         "color":"white"});
+
     // change background to gray
     // change font to white
 }
 
 function switchWhite() {
+  let body= $("body");
+  body.css("background" ,"white");
+    body.css("color","black");
     // change background to white
     // change font to black
 }
